2819641ae75eb59bf75a8d056e09c0b410b87ead
aaron-zaragoza <76231856+aaron-zaragoza@users.noreply.github.com>
2021-06-20 21:28:07 -0700

    First commit

---
+++ I_Am_Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_Am_Poor
+//
+//  Created by Aaron Zaragoza on 6/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
